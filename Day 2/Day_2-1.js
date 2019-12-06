4f26fdaea1c8d4a58f419116fa30aea3fabd38de
Ramon De La cruz <Ramon.DeLaCruz@ccconcern.org>
2019-12-05 16:02:12 -0800

    stubbed out Day 20-1 and started my prociutto code

--- Day 2/Day_2-1.js
+++ Day 2/Day_2-1.js
@@ -2,4 +2,16 @@
 
 function day2PlaceHolder() {
   console.log("I'm a placeholder!")
+
+//1 ADDS 2 and 3 and puts total at place 3 is number for. Then move forward 4 spaces
+//2 MULTIPLIES 2 and 3 and puts total at place 3 is number for. Then move forward 4 spaces
+var code = []
+
+  for (i = 0; i < code; i = i + 4) {
+    if (code[i] = 1) {
+      code[i + 3] = (code[i] + 2) + (code[i] + 3)
+    } else if (code[1] = 2) {
+      code[i + 3] = (code[i] + 2) * (code[i] + 3)
+    }
+  }
 }
